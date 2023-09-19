a4fd8c023b45e22af5a80d806b145b0004aff4c7
santosh <santoshbudha20@gmail.com>
2023-09-19 14:37:10 -0500

    backend shell scripting

---
+++ 03-vars.sh
@@ -0,0 +1,20 @@
+ #if we name some data is called a variable .
+
+
+ # syntax to decleare variable
+  vars_name = value of variable
+
+  x = 1000
+
+  name = devoops.
+
+
+   y = true
+
+
+  a = 20
+
+  #how to acess the variable
+
+  echo value of a = $a
+
